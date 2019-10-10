adc4409e133873fa22e41dea7c45f3b763095128
Charity Parks <tuppercharity@gmail.com>
2019-10-10 00:15:13 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,10 @@
 class Backer
   
-  attr_reader :name, :title
+  attr_reader :name, :backed_projects
   
   def initialize(name)
     @name = name
-    @title = title
+    @backed_projects = backed_projects
   end
   
   def backed_projects()
